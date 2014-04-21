3181cfb54bd3e56914748ab07bdcac752a39a321
Greg Payne <greg.payne@gmail.com>
2014-04-21 09:17:03 -0500

    Added license block template.

--- src/Tests/BasicTest.php
+++ src/Tests/BasicTest.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of the Splash package.
+ *
+ * (c) Greg Payne
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Splash\Tests;
 
 use Splash\Splash;
